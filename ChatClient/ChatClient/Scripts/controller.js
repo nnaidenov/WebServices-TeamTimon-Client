6bedf24f3a9d8eda2e97fc22466f87f35015df77
Ferdi Nebiev <ferdi.nebiev@gmail.com>
2013-08-14 18:32:47 +0300

    Created needed js files
    Successful test of pubnub

---
+++ ChatClient/ChatClient/Scripts/controller.js
@@ -0,0 +1,112 @@
+﻿/// <reference path="class.js" />
+/// <reference path="persisters.js" />
+/// <reference path="jquery-2.0.3.js" />
+/// <reference path="ui.js" />
+
+var controllers = (function () {
+
+    var rootUrl = "http://localhost:40643/api/";
+    var Controller = Class.create({
+        init: function () {
+            this.persister = persisters.get(rootUrl);
+        },
+        loadUI: function (selector) {
+            if (this.persister.isUserLoggedIn()) {
+                this.loadGameUI(selector);
+            }
+            else {
+                this.loadLoginFormUI(selector);
+            }
+            this.attachUIEventHandlers(selector);
+        },
+        loadLoginFormUI: function (selector) {
+            var loginFormHtml = ui.loginForm()
+            $(selector).html(loginFormHtml);
+        },
+        loadGameUI: function (selector) {
+            var self = this;
+            var gameUIHtml = ui.gameUI(this.persister.nickname());
+            $(selector).html(gameUIHtml);
+
+            this.attachUIEventHandlers(selector);
+        },
+        attachUIEventHandlers: function (selector) {
+            var wrapper = $(selector);
+            var self = this;
+
+            wrapper.on("click", "#btn-show-login", function () {
+                wrapper.find(".button.selected").removeClass("selected");
+                $(this).addClass("selected");
+                wrapper.find("#login-form").show();
+                wrapper.find("#register-form").hide();
+            });
+            wrapper.on("click", "#btn-show-register", function () {
+                wrapper.find(".button.selected").removeClass("selected");
+                $(this).addClass("selected");
+                wrapper.find("#register-form").show();
+                wrapper.find("#login-form").hide();
+            });
+
+            wrapper.on("click", "#btn-login", function () {
+                var user = {
+                    username: $(selector + " #tb-login-username").val(),
+                    password: $(selector + " #tb-login-password").val()
+                }
+
+                self.persister.user.login(user, function () {
+                    self.loadGameUI(selector);
+                }, function (err) {
+                    wrapper.find("#error-messages").text(err.responseJSON.Message);
+                });
+                return false;
+            });
+            wrapper.on("click", "#btn-register", function () {
+                var user = {
+                    username: $(selector).find("#tb-register-username").val(),
+                    nickname: $(selector).find("#tb-register-nickname").val(),
+                    password: $(selector + " #tb-register-password").val()
+                }
+                self.persister.user.register(user, function () {
+                    self.loadGameUI(selector);
+                }, function (err) {
+                    wrapper.find("#error-messages").text(err.responseJSON.Message);
+                });
+                return false;
+            });
+            wrapper.on("click", "#btn-logout", function () {
+                self.persister.user.logout(function () {
+                    self.loadLoginFormUI(selector);
+                    clearInterval(updateTimer);
+                }, function (err) {
+                });
+            });
+        },
+        setChatReceiver: function (channel, selector) {
+            var self = this;
+            this.persister.pubnub.subscribe(channel, function (message) {
+                $(selector).append(message);
+            });
+        },
+        setChatSender: function (channel, tb_selector, btn_selector) {
+            var self = this;
+            $(btn_selector).on("click", null, function (e) {
+                e.preventDefault();
+                var message = self.persister.username() + ": " + $(tb_selector).val() + '<br/>';
+                $(tb_selector).val('');
+                //console.log(message);
+                self.persister.pubnub.publish(channel, message);
+            });
+        }
+    });
+    return {
+        get: function () {
+            return new Controller();
+        }
+    }
+}());
+
+$(function () {
+    var controller = controllers.get();
+    controller.setChatReceiver("ferdi", "#content");
+    controller.setChatSender("ferdi", "#send-tb","#send-btn");
+});
---
+++ ChatClient/ChatClient/Scripts/persisters.js
@@ -0,0 +1,133 @@
+﻿/// <reference path="http-requester.js" />
+/// <reference path="class.js" />
+/// <reference path="http://crypto-js.googlecode.com/svn/tags/3.1.2/build/rollups/sha1.js" />
+/// <reference path="src="http://cdn.pubnub.com/pubnub-3.5.3.min.js" />
+
+var persisters = (function () {
+    var username = "ferdi";//localStorage.getItem("username"); !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+    var sessionKey = localStorage.getItem("sessionKey");
+
+    function saveUserData(userData) {
+        localStorage.setItem("username", userData.username);
+        localStorage.setItem("sessionKey", userData.sessionKey);
+        username = userData.username;
+        sessionKey = userData.sessionKey;
+    }
+    function clearUserData() {
+        localStorage.removeItem("username");
+        localStorage.removeItem("sessionKey");
+        nickname = "";
+        sessionKey = "";
+    }
+
+    var MainPersister = Class.create({
+        init: function (rootUrl) {
+            this.rootUrl = rootUrl;
+            this.user = new UserPersister(this.rootUrl);
+            this.chat = new ChatPersister(this.rootUrl);
+            this.pubnub = new PubnubPersister("demo", "demo");
+        },
+        isUserLoggedIn: function () {
+            var isLoggedIn = username != null && sessionKey != null;
+            return isLoggedIn;
+        },
+        username: function () {
+            return username;
+        }
+    });
+
+    var UserPersister = Class.create({
+        init: function (rootUrl) {
+            //...api/user/
+            this.rootUrl = rootUrl + "user/";
+        },
+        login: function (user, success, error) {
+            var url = this.rootUrl + "login";
+            var userData = {
+                username: user.username,
+                authCode: CryptoJS.SHA1(user.username + user.password).toString()
+            };
+
+            httpRequester.postJSON(url, userData,
+                function (data) {
+                    saveUserData(data);
+                    success(data);
+                }, error);
+        },
+        register: function (user, success, error) {
+            var url = this.rootUrl + "register";
+            var userData = {
+                username: user.username,
+                nickname: user.nickname,
+                authCode: CryptoJS.SHA1(user.username + user.password).toString()
+            };
+            httpRequester.postJSON(url, userData,
+                function (data) {
+                    saveUserData(data);
+                    success(data);
+                }, error);
+        },
+        logout: function (success, error) {
+            var url = this.rootUrl + "logout/" + sessionKey;
+            httpRequester.getJSON(url, function (data) {
+                clearUserData();
+                success(data);
+            }, error)
+        },
+        scores: function (success, error) {
+        }
+    });
+
+    var ChatPersister = Class.create({
+        init: function (rootUrl) {
+            this.rootUrl = rootUrl + "chat/";
+        },
+        create: function (userId, success, error) {
+            var url = this.rootUrl + "create/" + sessionKey;
+            var user = { userId: userId };
+            httpRequester.postJSON(url, user, success, error);
+        },
+        close: function (userId, success, error) {
+            var url = this.rootUrl + "close/" + sessionKey;
+            var user = { userId: userId };
+            httpRequester.postJSON(url, user, success, error);
+        }
+    });
+
+    var PubnubPersister = Class.create({
+        init: function (publish_key, subscribe_key) {
+            this.publish_key = publish_key;
+            this.subscribe_key = subscribe_key;
+
+            this.pubnub = PUBNUB.init({
+                publish_key: this.publish_key,
+                subscribe_key: this.subscribe_key,
+                ssl: true,
+            })
+
+            //this.pubnub = PUBNUB.secure({
+            //    publish_key: 'demo',
+            //    subscribe_key: 'demo',
+            //    ssl: true,
+            //    cipher_key: 'my-super-secret-password-key'
+            //});
+        },
+        subscribe: function (channel, success) {
+            this.pubnub.subscribe({
+                channel: channel,
+                message: success,
+            });
+        },
+        publish: function (channel, message) {
+            this.pubnub.publish({
+                channel: channel,
+                message: message
+            })
+        },
+    });
+    return {
+        get: function (url) {
+            return new MainPersister(url);
+        }
+    };
+}());
